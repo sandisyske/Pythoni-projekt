f1698079acfca1726453d5c5b987c69d3f7a3b36
sandisyske <sandraerik9@gmail.com>
2023-11-06 18:36:13 +0200

    Basic navigation

--- aardejaht.py
+++ aardejaht.py
@@ -20,7 +20,7 @@ pygame.display.set_caption("Aardejaht")
 WHITE = (255, 255, 255)
 
 #gravitatsioon, kui tahta kiiremini, siis saab seda väärtust tõsta
-GRAVITY = 1
+GRAVITY = 2
 
 #screen
 WIDTH, HEIGHT = 1000, 800
@@ -60,6 +60,15 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
 
     return all_sprites
 
+# bloki asukoht ja suurus
+def get_block(size):
+    path = join("Assets", "Backround", "terrain.png")
+    image = pygame.image.load(path).convert_alpha()
+    surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
+    # 0, 0 näitab vasakust äärest, kui kaugel on soovitud rect antud pildil
+    rect = pygame.Rect(0, 0, size, size)
+    surface.blit(image, (0, 0), rect)
+    return pygame.transform.scale2x(surface)
 
 
 #player
@@ -70,9 +79,10 @@ class Tegelane(pygame.sprite.Sprite):
     # kasuta sprite pilte
     SPRITES = load_sprite_sheets("Tegelased", "Rebane", 32, 32, True)
     # delay between sprites
-    ANIMATION_DELAY = 10
+    ANIMATION_DELAY =12
 
     def __init__(self, x, y, width, height):
+        super().__init__()
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
         self.y_vel = 0
@@ -80,7 +90,15 @@ class Tegelane(pygame.sprite.Sprite):
         self.direction = "left"
         self.animation_count = 0
         self.fall_count = 0
+        self.jump_count = 0
          
+    def jump(self):
+        self.y_vel = -self.GRAVITY * 6
+        self.animation_count = 0
+        self.jump_count += 1
+        if self.jump_count == 1:
+            self.fall_count = 0
+        
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -100,20 +118,43 @@ class Tegelane(pygame.sprite.Sprite):
             self.animation_count = 0
 
 
+
     def loop(self, fps):
-        #gravitatsiooni ei saa sise veel panna
-        #self.y_vel += min(1, self.fall_count/ fps) * self.GRAVITY
+        #gravitatsion
+        self.y_vel += min(1, self.fall_count/ fps) * self.GRAVITY
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
         self.update_sprite()
 
+    # funktsioonid landed ja hit_head blockiga collide'ides
+
+    def landed(self):
+        self.fall_count = 0 # stop adding gravity
+        self.y_vel = 0
+        self.jump_count = 0 #double jump reset
+
+
+    def hit_head(self):
+        self.count = 0
+        self.y_vel *= -1
 
     # animatsiooni uuendamine iga frame
     def update_sprite(self):
         sprite_sheet = "idle"
-        if self.x_vel != 0:
+        if self.y_vel < 0:
+            if self.jump_count == 1:
+                sprite_sheet = "jump"
+            elif self.jump_count == 2: # also i could add a cool salto triple jump animation
+                # double jump animation NEED HERE
+                sprite_sheet = "fall"
+        elif self.y_vel > self.GRAVITY * 2:
+            sprite_sheet = "fall"
+
+
+        elif self.x_vel != 0:
             sprite_sheet = "run"
+        
 
         sprite_sheet_name = sprite_sheet + "_" + self.direction
         sprites = self.SPRITES[sprite_sheet_name]
@@ -121,6 +162,8 @@ class Tegelane(pygame.sprite.Sprite):
                         self.ANIMATION_DELAY) % len(sprites)
         self.sprite = sprites[sprite_index]
         self.animation_count += 1
+        if sprite_sheet == "jump" and self.animation_count == 5:
+            sprite_sheet = "fall"
         self.update()
 
 
@@ -130,8 +173,30 @@ class Tegelane(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.sprite)
 
 
-    def draw(self, win):
-        win.blit(self.sprite, (self.rect.x, self.rect.y))
+    def draw(self, win, offset_x):
+        win.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
+
+# Tausta objektide parent klass
+class Object(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, name=None):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.width = width
+        self.height = height
+        self.name = name
+
+    def draw(self, win, offset_x):
+        win.blit(self.image, (self.rect.x - offset_x, self.rect.y))
+
+
+ # blokki child klass 
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
+        block = get_block(size)
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
 
 
 
@@ -149,30 +214,79 @@ def taust(name):
 
 
 
-def draw(window, backround, bg_image, rebane):
+def draw(window, backround, bg_image, rebane, objects, offset_x):
 
     for tile in backround:
         window.blit(bg_image, tile)
 
-    rebane.draw(window)
+    for obj in objects:
+        obj.draw(window, offset_x)
+
+    rebane.draw(window, offset_x)
 
     pygame.display.update()
 
-def handle_move(rebane):
+def handle_vertical_collision(rebane, objects, dy):
+    collided_objects = []
+    for obj in objects:
+        if pygame.sprite.collide_mask(rebane, obj):
+            #hitting top of bottom of the object differently
+            if dy > 0:
+                rebane.rect.bottom = obj.rect.top
+                rebane.landed()
+            elif dy < 0:
+                rebane.rect.top = obj.rect.bottom
+                rebane.hit_head()
+        collided_objects.append(obj)
+
+    return collided_objects
+
+def collide(rebane, objects, dx):
+    rebane.move(dx, 0)
+    rebane.update()
+    collided_object = None
+    for obj in objects:
+        if pygame.sprite.collide_mask(rebane, obj):
+            collided_object = obj
+            break
+
+    rebane.move(-dx, 0)
+    rebane.update()
+    return collided_object
+
+
+
+
+#move ja collision
+def handle_move(rebane, objects):
     keys = pygame.key.get_pressed()
 
     rebane.x_vel = 0
-    if keys[pygame.K_LEFT]:
+    # kui hakkab glitchima vastu blokki saab lisada PLAYER_VEL * 2
+    collide_left = collide(rebane, objects, -PLAYER_VEL*2)
+    collide_right = collide(rebane, objects, PLAYER_VEL*2)
+
+    if keys[pygame.K_LEFT] and not collide_left:
         rebane.move_left(PLAYER_VEL)
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and not collide_right:
         rebane.move_right(PLAYER_VEL)
+
+    handle_vertical_collision(rebane, objects, rebane.y_vel)
 
 
 def main (window):
     clock = pygame.time.Clock()
     backround, bg_image = taust("taust.png")
 
+    block_size = 48
+
     rebane = Tegelane(100, 100, 50, 50)
+    floor = [Block(i*block_size, HEIGHT - block_size, block_size) 
+             for i in range (-WIDTH // block_size, (WIDTH * 2) // block_size)]
+    objects = [*floor, Block(0, HEIGHT - block_size * 2, block_size), 
+               Block(block_size * 3, HEIGHT - block_size * 4, block_size)]
+    offset_x = 0
+    scroll_area_width = 200
 
     run = True
     while run:
@@ -184,10 +298,18 @@ def main (window):
             if event.type == pygame.QUIT:
                 run = False
                 break
-        
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and rebane.jump_count < 2:
+                    rebane.jump()
+
         rebane.loop(FPS)
-        handle_move(rebane)
-        draw(window, backround, bg_image, rebane)
+        handle_move(rebane, objects)
+        draw(window, backround, bg_image, rebane, objects, offset_x)
+
+        if ((rebane.rect.right - offset_x >= WIDTH - scroll_area_width) and rebane.x_vel > 0) or (
+                (rebane.rect.left - offset_x <= scroll_area_width) and rebane.x_vel < 0):
+            offset_x += rebane.x_vel
 
     pygame.quit()
     quit()
