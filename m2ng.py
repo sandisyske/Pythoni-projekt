493dc4952ed605c121dcf49f0b3838a16943af6f
Sandra Erik <sandisyske@gmail.com>
2023-11-14 18:53:05 +0200

    tavalised ilmutised ja kummitused

--- m2ng.py
+++ m2ng.py
@@ -10,14 +10,52 @@ class Game:
 
         self.clock = pygame.time.Clock() # ekraani nimi
 
-        self.img = pygame.image.load("data/images/clouds/cloud_1")
+        self.img = pygame.image.load("data/images/clouds/cloud_1.png")
+        self.img.set_colorkey((0, 0, 0))
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
 
+        self.collision_area = pygame.Rect(50, 50, 300, 50) # test collisions
     def run(self):
         while True:
+
+            #clear backround
+            self.screen.fill((12, 220, 250))
+
+            #COLLISIONS
+            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+            # shortcut  #pygame.Rect(*self.img_pos, *self.img.get_size())
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
+            else:
+                pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
+
+            
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5 # korrutis on liikumiskiirus
+            self.screen.blit(self.img, self.img_pos)
+
+    
+
+
+            # EVENTS
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
+                    pygame.quit() 
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False       
+
+                 
+            
     
             pygame.display.update()
             self.clock.tick(60)  #FPS
